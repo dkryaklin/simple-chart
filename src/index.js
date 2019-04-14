5df4d12fd9db6e6fe0aa99c9122911b37d681649
Dmitry Kryaklin <dmitry.kryaklin@behavox.com>
2019-04-14 16:54:56 +0300

    remove old files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-new */
-// new window.Chart(document.body, 500, 600, '/data/1');
-// new window.Chart(document.body, 500, 600, '/data/2');
-// new window.Chart(document.body, 500, 600, '/data/3');
-// new window.Chart(document.body, 500, 600, '/data/4');
-// new window.Chart(document.body, 500, 600, '/data/5');
